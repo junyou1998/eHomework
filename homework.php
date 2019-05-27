1c85b91214ad0aed7a1ceea4ed4084563e221419
junyou1998 <junyou1998@gmail.com>
2019-05-27 20:38:01 +0800

    update

---
+++ homework.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title></title>
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css?family=Noto+Sans+TC:300,400,500,700,900&display=swap&subset=chinese-traditional" rel="stylesheet">
+  <link rel="stylesheet" href="style/homework.css">
+</head>
+
+<body>
+  <h1>所有作業</h1>
+  <div class="content">
+  </div>
+  <div class="card">
+    <h2>吉祥物繪製</h2>
+    <p>需有草圖以及清稿，並使用AI繪製將其數位化。
+    </p>
+    <div class="tags">
+      <span class="tag">基本設計</span>
+      <span class="tag">106上學期</span>
+    </div>
+  </div>
+  <div class="card">
+    <h2>吉祥物繪製</h2>
+    <p>需有草圖以及清稿，並使用AI繪製將其數位化。
+    </p>
+    <div class="tags">
+      <span class="tag">基本設計</span>
+      <span class="tag">106上學期</span>
+    </div>
+  </div>
+  <div class="card">
+    <h2>吉祥物繪製</h2>
+    <p>需有草圖以及清稿，並使用AI繪製將其數位化。
+    </p>
+    <div class="tags">
+      <span class="tag">基本設計</span>
+      <span class="tag">106上學期</span>
+    </div>
+  </div>
+  <div class="card">
+    <h2>吉祥物繪製</h2>
+    <p>需有草圖以及清稿，並使用AI繪製將其數位化。
+    </p>
+    <div class="tags">
+      <span class="tag">基本設計</span>
+      <span class="tag">106上學期</span>
+    </div>
+  </div>
+  <div class="card">
+    <h2>吉祥物繪製</h2>
+    <p>需有草圖以及清稿，並使用AI繪製將其數位化。
+    </p>
+    <div class="tags">
+      <span class="tag">基本設計</span>
+      <span class="tag">106上學期</span>
+    </div>
+  </div>
+  <div class="card">
+    <h2>吉祥物繪製</h2>
+    <p>需有草圖以及清稿，並使用AI繪製將其數位化。
+    </p>
+    <div class="tags">
+      <span class="tag">基本設計</span>
+      <span class="tag">106上學期</span>
+    </div>
+  </div>
+  <div class="panel">
+    <i class="fas fa-book-open"></i>
+    <i class="fas fa-edit"></i>
+    <i class="fas fa-calendar-check"></i>
+    <i class="fas fa-user"></i>
+  </div>
+</body>
+
+</html>
---
+++ upload.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title></title>
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css?family=Noto+Sans+TC:300,400,500,700,900&display=swap&subset=chinese-traditional" rel="stylesheet">
+  <link rel="stylesheet" href="style/upload.css">
+</head>
+
+<body>
+  <h1>作業繳交</h1>
+  <div class="content">
+  </div>
+  <div class="card">
+    <div class="tags">
+      <span class="tag">基本設計</span>
+      <span class="tag">106上學期</span>
+    </div>
+    <h3>作業說明</h3>
+    <p>需有草圖以及清稿，並使用AI繪製將其數位化。</p>
+    <div class="upload">
+      <h2>附加檔案上傳</h2>
+      <i class="fas fa-cloud-upload-alt"></i>
+    </div>
+    <form action="">
+      <textarea cols="30" rows="5"></textarea>
+    </form>
+    <input id="handup" type="submit" value="繳交">
+  </div>
+  <div class="panel">
+    <i class="fas fa-book-open"></i>
+    <i class="fas fa-edit"></i>
+    <i class="fas fa-calendar-check"></i>
+    <i class="fas fa-user"></i>
+  </div>
+</body>
+
+</html>
